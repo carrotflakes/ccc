8496d2844e5f32150fbb24401e8dc6d035c464b1
carrotflakes <carrotflakes@gmail.com>
2019-02-27 23:34:41 +0900

    12 + 34 - 5 => 41

--- ccc.c
+++ ccc.c
@@ -1,5 +1,56 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+enum {
+  TK_NUM = 256,
+  TK_EOF,
+};
+
+typedef struct {
+  int ty;
+  int val;
+  char *input;
+} Token;
+
+Token tokens[100];
+
+void tokenize(char *p) {
+  int i = 0;
+  while (*p) {
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    if (*p == '+' || *p == '-') {
+      tokens[i].ty = *p;
+      tokens[i].input = p;
+      i++;
+      p++;
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      tokens[i].ty = TK_NUM;
+      tokens[i].input = p;
+      tokens[i].val = strtol(p, &p, 10);
+      i++;
+      continue;
+    }
+    fprintf(stderr, "Tokenize failed: %s\n", p);
+    exit(1);
+  }
+
+  tokens[i].ty = TK_EOF;
+  tokens[i].input = p;
+}
+
+void error(int i) {
+  fprintf(stderr, "Unexpected token: %s\n", tokens[i].input);
+  exit(1);
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -7,10 +58,39 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  tokenize(argv[1]);
+
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  printf("\tmov rax, %d\n", atoi(argv[1]));
-  printf("\tret\n");
+
+  if (tokens[0].ty != TK_NUM)
+    error(0);
+  printf("  mov rax, %d\n", tokens[0].val);
+
+  int i = 1;
+  while (tokens[i].ty != TK_EOF) {
+    if (tokens[i].ty == '+') {
+      i++;
+      if (tokens[i].ty != TK_NUM)
+        error(i);
+      printf("  add rax, %d\n", tokens[i].val);
+      i++;
+      continue;
+    }
+
+    if (tokens[i].ty == '-') {
+      i++;
+      if (tokens[i].ty != TK_NUM)
+        error(i);
+      printf("  sub rax, %d\n", tokens[i].val);
+      i++;
+      continue;
+    }
+
+    error(i);
+  }
+
+  printf("  ret\n");
   return 0;
 }
